79fbd630f4af8be84351844b496687d04ac4f074
all persons <35461002238898@mail.ru>
2021-04-01 12:25:31 +0300

    refactoring code

--- attachment/js/script.js
+++ attachment/js/script.js
@@ -2,57 +2,50 @@ const inputName = document.getElementById('input_name');
 const inputLastName = document.getElementById('input_last_name');
 const inputAge = document.getElementById('input_age');
 const openModalWindowBtn = document.getElementById('open_window_btn');
-const ERROR_MESSAGE = document.getElementById('error_message');
+const ERROR_MESSAGE_NAME = document.getElementById('error_message');
+const ERROR_MESSAGE_LAST_NAME = document.getElementById('error_message');
 const cancelModalWindow = document.getElementById('cancel_modal_window');
 const addUserDataBtn = document.getElementById('add_user_data');
 const modalWindow = document.getElementById('modal_window');
 const ERROR_NUMBER = document.getElementById('error_message_number');
 
-function hideModalWindowFn() {
-    return ERROR_MESSAGE.classList.add('hide');
+function hideErrorClassListModalWindow() {
+    return ERROR_MESSAGE_NAME.classList.add('hide');
 };
 
-function openModalWindowFn() {
-    return ERROR_MESSAGE.classList.remove('hide');
+function showErrorClassListModalWindow() {
+    return ERROR_MESSAGE_NAME.classList.remove('hide');
 };
 
 window.addEventListener('keydown', event => {
     if (event.code === 'Escape') {
-        hideModalWindowFn()
+        hideErrorClassListModalWindow()
     }
 });
 
+
 function checkValidInputText(event) {
-    const errorElement = document.createElement('p');
-    errorElement.innerHTML = `Error! Enter correct data!`;
     if (event.target.value.length > 20) {
-                openModalWindowFn();
-        addUserDataBtn.disabled = true;
-        // event.target.prepend(errorElement)
-    } else if (!isNaN(event.target.value)) {
-        openModalWindowFn();
-        addUserDataBtn.disabled = true;
+        event.target.nextElementSibling.classList.remove('hide');
     } else if (event.target.value.length < 20) {
-        addUserDataBtn.disabled = false;
-        hideModalWindowFn()
+        event.target.nextElementSibling.classList.add('hide');
     } else {
-    addUserDataBtn.disabled = true;
-        hideModalWindowFn();
+        event.target.nextElementSibling.classList.add('hide');
     }
 };
 
 function checkValidInputNumber(event) {
     if (isNaN(Number(event.target.value))) {
         ERROR_NUMBER.classList.remove('hide');
-        addUserDataBtn.disabled = true;
-    } else if(event.target.value === '') {
-        addUserDataBtn.disabled = true;
-    }
-    else if (!isNaN(event.target.value)) {
-        addUserDataBtn.disabled = false;
+    } else if (!(Number(event.target.value))) {
         ERROR_NUMBER.classList.add('hide');
-    } 
+    } else if (event.target.value.length > 20) {
+        ERROR_NUMBER.classList.remove('hide');
+    } else {
+        ERROR_NUMBER.classList.add('hide');
+    }
 };
+
 
 function checkEmptyInputs() {
     inputName,
